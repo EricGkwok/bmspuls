ed2ba5fc63fbdb8c8674eb28a8745fc54bfc00f4
ice <1499046615@qq.com>
2019-07-05 17:28:08 +0800

    ice 7

---
+++ src/main/java/bms/repository/SysDeptRepository.java
@@ -0,0 +1,7 @@
+package bms.repository;
+
+import bms.entity.SysDept;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface SysDeptRepository extends JpaRepository<SysDept,String> {
+}
