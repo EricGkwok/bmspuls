6eeafee54ef481bf3ac13cc59a0ded30c95c51ab
ice <1499046615@qq.com>
2019-07-05 17:06:40 +0800

    ice 2

--- src/test/java/m/bmspuls/bmspuls/BmspulsApplicationTests.java
+++ src/test/java/m/bmspuls/bmspuls/BmspulsApplicationTests.java
@@ -6,11 +6,13 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@SpringBootTest(classes=BmspulsApplicationTests.class)
 public class BmspulsApplicationTests {
 
     @Test
     public void contextLoads() {
+
+        System.out.println("ok");
     }
 
 }
