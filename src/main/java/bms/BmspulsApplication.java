2ea096b012e39245e403fa33378d8c1ee35bb845
EricGkwok <18871347405@163.com>
2019-07-05 15:07:35 +0800

    Initial commit

---
+++ src/main/java/bms/BmspulsApplication.java
@@ -0,0 +1,13 @@
+package bms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BmspulsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BmspulsApplication.class, args);
+    }
+
+}
---
+++ src/main/java/bms/controller/TestController.java
@@ -0,0 +1,20 @@
+package bms.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+@RestController
+public class TestController {
+
+        @RequestMapping("sel")
+       String findAll(){
+            return "404";
+        }
+
+
+
+
+}
